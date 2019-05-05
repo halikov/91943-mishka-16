58553528d84b15d0769d8773d1dc44bd2df314b3
Mikail Halikov <mikail@halikov.com>
2019-05-05 21:36:58 +0300

    исправляет ошибки опечатки и тп. Изменяет путь для иконок спрайта инлайновые не отображаются, с внешнего файла отображаются.

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,11 @@ gulp.task("images", function() {
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
+      imagemin.svgo({
+        plugins: [
+          {cleanupIDs: true}
+        ]
+      })
     ]))
     .pipe(gulp.dest("build/img"));
 });
