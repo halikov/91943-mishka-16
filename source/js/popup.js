3164c95883651eff6741a49fec8a722ea4d6b656
Mikail Halikov <mikail@halikov.com>
2019-05-10 23:15:39 +0300

    Добавляет открытие попапа по нажатию на все кнопки заказа. В каталоге заменяет инлайновый svg на спрайт

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,20 +1,17 @@
-var orderBtns = document.querySelector('.order-button');
+var orderBtns = document.querySelectorAll('.order-button');
 var popup = document.querySelector('.popup');
 var close = popup.querySelector('.close');
 
-orderBtns.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  popup.classList.toggle('popup--closed');
-});
+for (var i = 0; i < orderBtns.length; i++) {
+  var currentBtn = orderBtns[i];
+
+  currentBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popup.classList.toggle('popup--closed');
+  });
+}
 
 close.addEventListener('click', function(evt) {
   evt.preventDefault();
   popup.classList.add('popup--closed');
-
-  window.addEventListener('keydown', function(evt) {
-    if(evt.keycode === 27) {
-      evt.preventDefault();
-      popup.classList.add('popup--closed')
-    }
-  })
 });
